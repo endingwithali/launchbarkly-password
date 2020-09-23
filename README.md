d9a946d14eff2a66564ab574276463a490de8f4e
asfink <ali@finkelstein.us>
2020-09-23 13:55:08 -0700

    Server works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # launchbarkly-password
+
+
+This server is to exclusively check the status of the LaunchBarkly password flag. 
+
+Must use at minimum node 13.14.0 or else there will be errors
--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from 'express'
 import LaunchDarkly from 'launchdarkly-node-server-sdk'
-import bcrypt from 'bcrypt'
-import pbkdf2 from 'pbkdf2'
 import dotenv from 'dotenv'
 
 dotenv.config();
@@ -9,52 +7,34 @@ const PORT = process.env.PORT || 3000
 const app = express();
 const ldClient = LaunchDarkly.init(process.env.LD_PROD_KEY);
 
-const user = {
-    key :"server"
- };
-
-
-//THIS WORKS!!!
 ldClient.once("ready", () => {
+
+    /**
+     * Checks to see what method the application should be use for password hashing 
+     * 
+     * @return {boolean} true = use bcrypt
+     * @return {boolean} false = use pbkdf2
+     * 
+     */
     app.get('/', function(req, res){
-        ldClient.variation("launchbarkly", {"key": "user@test.com"}, false,
+        ldClient.variation("launchbarkly", {"key": "server"}, false,
         (err, showFeature) => {
-            console.log(err)
             if (showFeature) {
-                console.log("here")
-            // application code to show the feature
+                res.send('bcrypt')
             } else {
-                console.log('there')
-            // the code to run if the feature is off
+                res.send('pbkdf2')
             }
         });
-        res.send('POST');
     })
 
     app.listen(PORT, () => {
         console.log(`Listening on ${ PORT }`)
-        // hash("b")
     });
-    // ldClient.variation("launchbarkly", {"key": "user@test.com"}, false,
-    //   (err, showFeature) => {
-    //       console.log(err)
-    //     if (showFeature) {
-    //         console.log("here")
-    //       // application code to show the feature
-    //     } else {
-    //         console.log('there')
-    //       // the code to run if the feature is off
-    //     }
-    //   });
+
+    process.on('SIGINT', function() {
+        console.log('quitting...');
+        ldClient.close()
+        process.exit(0);
+      });
   });
-
-
-// process.on('SIGTERM', () => {
-//     console.info('SIGTERM signal received.');
-//     console.log('Closing http server.');
-//     ldClient.close();
-//     server.close(() => {
-//       console.log('Http server closed.');
-//     });
-// });
 
